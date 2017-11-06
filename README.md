fd265fa1a20c53e37e29d30f03d7102818adc290
matyix <janos.matyas@gmail.com>
2017-11-06 18:58:39 +0100

    Pipeline API client plugin for Drone

---
+++ README.md
@@ -0,0 +1,59 @@
+## Pipeline API client plugin for Drone
+
+### Example drone config
+
+.drone.yml
+
+    pipeline:
+      cluster:
+        image: plugins/pipeline_plugin:latest
+
+        endpoint: http://[your-host-name-or-ip]/pipeline/api/v1/
+        username: admin
+        password: *****
+        log_level: info
+        log_format: text
+
+        cluster_name: "test-cluster"
+        cluster_location: "eu-west-1"
+        cluster_state: "present"
+
+        node_image: ami-34b6764d
+        node_instance_type: m4.xlarge
+
+        master_image: ami-34b6764d
+        master_instance_type: m4.xlarge
+
+        deployment_name: "zeppelin-k8s-charts"
+        deployment_stat: "present"
+
+## Test container/plugin with drone exec
+    drone exec --repo-name hello-world --workspace-path drone-test .drone.yml
+## Build new docker image
+
+    make docker
+
+## For dev env push .env file
+
+.env
+
+    PLUGIN_CLUSTER_NAME=test-cluster
+    PLUGIN_CLUSTER_LOCATION="eu-west-1"
+    PLUGIN_CLUSTER_STATE="present"
+
+    PLUGIN_NODE_IMAGE=ami-34b6764d
+    PLUGIN_NODE_INSTANCE_TYPE=m4.xlarge
+
+    PLUGIN_MASTER_IMAGE=ami-34b6764d
+    PLUGIN_MASTER_INSTANCE_TYPE=m4.xlarge
+
+    PLUGIN_ENDPOINT=http://[your-host-name-or-ip]/pipeline/api/v1/
+    PLUGIN_USERNAME=admin
+    PLUGIN_PASSWORD=***
+    PLUGIN_LOG_LEVEL=debug
+    PLUGIN_DEPLOYMENT_NAME="zeppelin-k8s-charts"
+    PLUGIN_DEPLOYMENT_STATE="present"
+
+### Test with `go run`
+
+    go run -ldflags "-X main.version=1.0" main.go plugin.go log.go --plugin.log.level debug --plugin.log.format text
---
+++ main.go
@@ -0,0 +1,369 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/urfave/cli"
+	"os"
+)
+
+var (
+	version string = ""
+)
+
+func main() {
+
+	_ = godotenv.Load()
+
+	app := cli.NewApp()
+	app.Name = "pipeline plugin"
+	app.Usage = "pipeline plugin"
+	app.Action = run
+	app.Version = fmt.Sprintf("%s", version)
+	app.EnableBashCompletion = true
+	app.Flags = []cli.Flag{
+
+		//
+		// repo args
+		//
+
+		cli.StringFlag{
+			Name:   "repo.fullname",
+			Usage:  "repository full name",
+			EnvVar: "DRONE_REPO",
+		},
+		cli.StringFlag{
+			Name:   "repo.owner",
+			Usage:  "repository owner",
+			EnvVar: "DRONE_REPO_OWNER",
+		},
+		cli.StringFlag{
+			Name:   "repo.name",
+			Usage:  "repository name",
+			EnvVar: "DRONE_REPO_NAME",
+		},
+		cli.StringFlag{
+			Name:   "repo.link",
+			Usage:  "repository link",
+			EnvVar: "DRONE_REPO_LINK",
+		},
+		cli.StringFlag{
+			Name:   "repo.avatar",
+			Usage:  "repository avatar",
+			EnvVar: "DRONE_REPO_AVATAR",
+		},
+		cli.StringFlag{
+			Name:   "repo.branch",
+			Usage:  "repository default branch",
+			EnvVar: "DRONE_REPO_BRANCH",
+		},
+		cli.BoolFlag{
+			Name:   "repo.private",
+			Usage:  "repository is private",
+			EnvVar: "DRONE_REPO_PRIVATE",
+		},
+		cli.BoolFlag{
+			Name:   "repo.trusted",
+			Usage:  "repository is trusted",
+			EnvVar: "DRONE_REPO_TRUSTED",
+		},
+
+		//
+		// commit args
+		//
+
+		cli.StringFlag{
+			Name:   "remote.url",
+			Usage:  "git remote url",
+			EnvVar: "DRONE_REMOTE_URL",
+		},
+		cli.StringFlag{
+			Name:   "commit.sha",
+			Usage:  "git commit sha",
+			EnvVar: "DRONE_COMMIT_SHA",
+		},
+		cli.StringFlag{
+			Name:   "commit.ref",
+			Value:  "refs/heads/master",
+			Usage:  "git commit ref",
+			EnvVar: "DRONE_COMMIT_REF",
+		},
+		cli.StringFlag{
+			Name:   "commit.branch",
+			Value:  "master",
+			Usage:  "git commit branch",
+			EnvVar: "DRONE_COMMIT_BRANCH",
+		},
+		cli.StringFlag{
+			Name:   "commit.message",
+			Usage:  "git commit message",
+			EnvVar: "DRONE_COMMIT_MESSAGE",
+		},
+		cli.StringFlag{
+			Name:   "commit.link",
+			Usage:  "git commit link",
+			EnvVar: "DRONE_COMMIT_LINK",
+		},
+		cli.StringFlag{
+			Name:   "commit.author.name",
+			Usage:  "git author name",
+			EnvVar: "DRONE_COMMIT_AUTHOR",
+		},
+		cli.StringFlag{
+			Name:   "commit.author.email",
+			Usage:  "git author email",
+			EnvVar: "DRONE_COMMIT_AUTHOR_EMAIL",
+		},
+		cli.StringFlag{
+			Name:   "commit.author.avatar",
+			Usage:  "git author avatar",
+			EnvVar: "DRONE_COMMIT_AUTHOR_AVATAR",
+		},
+
+		//
+		// build args
+		//
+
+		cli.StringFlag{
+			Name:   "build.event",
+			Value:  "push",
+			Usage:  "build event",
+			EnvVar: "DRONE_BUILD_EVENT",
+		},
+		cli.IntFlag{
+			Name:   "build.number",
+			Usage:  "build number",
+			EnvVar: "DRONE_BUILD_NUMBER",
+		},
+		cli.IntFlag{
+			Name:   "build.created",
+			Usage:  "build created",
+			EnvVar: "DRONE_BUILD_CREATED",
+		},
+		cli.IntFlag{
+			Name:   "build.started",
+			Usage:  "build started",
+			EnvVar: "DRONE_BUILD_STARTED",
+		},
+		cli.IntFlag{
+			Name:   "build.finished",
+			Usage:  "build finished",
+			EnvVar: "DRONE_BUILD_FINISHED",
+		},
+		cli.StringFlag{
+			Name:   "build.status",
+			Usage:  "build status",
+			Value:  "success",
+			EnvVar: "DRONE_BUILD_STATUS",
+		},
+		cli.StringFlag{
+			Name:   "build.link",
+			Usage:  "build link",
+			EnvVar: "DRONE_BUILD_LINK",
+		},
+		cli.StringFlag{
+			Name:   "build.deploy",
+			Usage:  "build deployment target",
+			EnvVar: "DRONE_DEPLOY_TO",
+		},
+		cli.BoolFlag{
+			Name:   "yaml.verified",
+			Usage:  "build yaml is verified",
+			EnvVar: "DRONE_YAML_VERIFIED",
+		},
+		cli.BoolFlag{
+			Name:   "yaml.signed",
+			Usage:  "build yaml is signed",
+			EnvVar: "DRONE_YAML_SIGNED",
+		},
+
+		//
+		// prev build args
+		//
+
+		cli.IntFlag{
+			Name:   "prev.build.number",
+			Usage:  "previous build number",
+			EnvVar: "DRONE_PREV_BUILD_NUMBER",
+		},
+		cli.StringFlag{
+			Name:   "prev.build.status",
+			Usage:  "previous build status",
+			EnvVar: "DRONE_PREV_BUILD_STATUS",
+		},
+		cli.StringFlag{
+			Name:   "prev.commit.sha",
+			Usage:  "previous build sha",
+			EnvVar: "DRONE_PREV_COMMIT_SHA",
+		},
+
+		//
+		// plugin args
+		//
+
+		cli.StringFlag{
+			Name:   "plugin.endpoint",
+			Usage:  "API Url",
+			EnvVar: "PLUGIN_ENDPOINT",
+		},
+		cli.StringFlag{
+			Name:   "plugin.username",
+			Usage:  "API Username",
+			EnvVar: "PLUGIN_USERNAME",
+		},
+		cli.StringFlag{
+			Name:   "plugin.password",
+			Usage:  "API Password",
+			EnvVar: "PLUGIN_PASSWORD",
+		},
+		cli.StringFlag{
+			Name:   "plugin.cluster.name",
+			Usage:  "K8S Cluster name",
+			EnvVar: "PLUGIN_CLUSTER_NAME",
+		},
+		cli.StringFlag{
+			Name:   "plugin.cluster.location",
+			Usage:  "Specific aws region",
+			EnvVar: "PLUGIN_CLUSTER_LOCATION",
+		},
+		cli.StringFlag{
+			Name:   "plugin.cluster.state",
+			Usage:  "K8S cluster desire state",
+			EnvVar: "PLUGIN_CLUSTER_STATE",
+			Value:  "present",
+		},
+		cli.StringFlag{
+			Name:   "plugin.node.image",
+			Usage:  "Amazon machine image id",
+			EnvVar: "PLUGIN_NODE_IMAGE",
+		},
+		cli.StringFlag{
+			Name:   "plugin.node.instanceType",
+			Usage:  "EC2 instance type",
+			EnvVar: "PLUGIN_NODE_INSTANCE_TYPE",
+		},
+		cli.IntFlag{
+			Name:   "plugin.node.minCount",
+			Usage:  "Minimum number of node",
+			EnvVar: "PLUGIN_NODE_MIN_COUNT",
+			Value:  1,
+		},
+		cli.IntFlag{
+			Name:   "plugin.node.maxCount",
+			Usage:  "Maximum number of node",
+			EnvVar: "PLUGIN_NODE_MAX_COUNT",
+			Value:  1,
+		},
+		cli.Float64Flag{
+			Name:   "plugin.node.spotPrice",
+			Usage:  "Spot price",
+			EnvVar: "PLUGIN_NODE_SPOT_PRICE",
+			Value:  0,
+		},
+		cli.StringFlag{
+			Name:   "plugin.master.image",
+			Usage:  "Amazon machine image id",
+			EnvVar: "PLUGIN_MASTER_IMAGE",
+		},
+		cli.StringFlag{
+			Name:   "plugin.master.instanceType",
+			Usage:  "EC2 instance type",
+			EnvVar: "PLUGIN_MASTER_INSTANCE_TYPE",
+		},
+		cli.StringFlag{
+			Name:   "plugin.deployment.name",
+			Usage:  "Specific deployment name",
+			EnvVar: "PLUGIN_DEPLOYMENT_NAME",
+		},
+		cli.StringFlag{
+			Name:   "plugin.deployment.state",
+			Usage:  "Specific deployment state",
+			EnvVar: "PLUGIN_DEPLOYMENT_STATE",
+		},
+
+		cli.StringFlag{
+			Name:   "plugin.log.level",
+			Usage:  "Specific log level (debug,info,warn)",
+			EnvVar: "PLUGIN_LOG_LEVEL",
+		},
+		cli.StringFlag{
+			Name:   "plugin.log.format",
+			Usage:  "Specific log format (text, json) default is text",
+			EnvVar: "PLUGIN_LOG_FORMAT",
+			Value:  "text",
+		},
+	}
+	app.Run(os.Args)
+}
+
+func run(c *cli.Context) error {
+
+	plugin := Plugin{
+		Repo: Repo{
+			Owner:   c.String("repo.owner"),
+			Name:    c.String("repo.name"),
+			Link:    c.String("repo.link"),
+			Avatar:  c.String("repo.avatar"),
+			Branch:  c.String("repo.branch"),
+			Private: c.Bool("repo.private"),
+			Trusted: c.Bool("repo.trusted"),
+		},
+		Build: Build{
+			Number:   c.Int("build.number"),
+			Event:    c.String("build.event"),
+			Status:   c.String("build.status"),
+			Deploy:   c.String("build.deploy"),
+			Created:  int64(c.Int("build.created")),
+			Started:  int64(c.Int("build.started")),
+			Finished: int64(c.Int("build.finished")),
+			Link:     c.String("build.link"),
+		},
+		Commit: Commit{
+			Remote:  c.String("remote.url"),
+			Sha:     c.String("commit.sha"),
+			Ref:     c.String("commit.sha"),
+			Link:    c.String("commit.link"),
+			Branch:  c.String("commit.branch"),
+			Message: c.String("commit.message"),
+			Author: Author{
+				Name:   c.String("commit.author.name"),
+				Email:  c.String("commit.author.email"),
+				Avatar: c.String("commit.author.avatar"),
+			},
+		},
+		Config: Config{
+			Endpoint: c.String("plugin.endpoint"),
+			Username: c.String("plugin.username"),
+			Password: c.String("plugin.password"),
+			Cluster: Cluster{
+				Name:     c.String("plugin.cluster.name"),
+				Location: c.String("plugin.cluster.location"),
+				State:    c.String("plugin.cluster.state"),
+				Node: Node{
+					Image:        c.String("plugin.node.image"),
+					InstanceType: c.String("plugin.node.instanceType"),
+					MinCount:     c.Int("plugin.node.minCount"),
+					MaxCount:     c.Int("plugin.node.maxCount"),
+					SpotPrice:    c.Float64("plugin.node.spotPrice"),
+				},
+				Master: Master{
+					Image:        c.String("plugin.master.image"),
+					InstanceType: c.String("plugin.master.instanceType"),
+				},
+				Deployment: Deployment{
+					Name:  c.String("plugin.deployment.name"),
+					State: c.String("plugin.deployment.state"),
+				},
+			},
+		},
+	}
+
+	SetLogLevel(c.String("plugin.log.level"))
+	SetLogFormat(c.String("plugin.log.format"))
+
+	err := plugin.Exec()
+	if err != nil {
+		Fatal(err)
+	}
+	return nil
+}
